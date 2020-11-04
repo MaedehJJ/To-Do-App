82949cc3f06a44008e84a3d6cec2c609fabdce2f
MaedehJJ <majj2000.mail@gmail.com>
2020-11-04 18:31:02 +0330

    Create : Adapter for recyclerView

---
+++ app/src/main/java/com/example/todoapp/fragments/list/ListAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.todoapp.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.R
+import com.example.todoapp.database.models.Priority
+import com.example.todoapp.database.models.ToDoData
+import kotlinx.android.synthetic.main.row_list.view.*
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+    var dataList = ArrayList<ToDoData>()
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListAdapter.ViewHolder, position: Int) {
+        holder.itemView.titleTxt.text = dataList[position].title
+        holder.itemView.descriptionTxt.text = dataList[position].description
+        val priority = dataList[position].priority
+        when (priority) {
+            Priority.High -> holder.itemView.priorityIndicator.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.red
+                )
+            )
+            Priority.Medium -> holder.itemView.priorityIndicator.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.yellow
+                )
+            )
+            Priority.Low -> holder.itemView.priorityIndicator.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.green
+                )
+            )
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    fun setData(toDoData: List<ToDoData>) {
+        this.dataList = toDoData as ArrayList
+        this.notifyDataSetChanged()
+    }
+
+}
+
+
