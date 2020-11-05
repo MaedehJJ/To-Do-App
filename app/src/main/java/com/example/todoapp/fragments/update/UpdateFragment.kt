bed7b586668a073377150bbb94ea4c3137040fe7
MaedehJJ <majj2000.mail@gmail.com>
2020-11-05 19:40:12 +0330

    Add : pass todo data object to UpdateFragment.kt

--- app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
@@ -3,16 +3,27 @@ package com.example.todoapp.fragments.update
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.todoapp.R
+import com.example.todoapp.database.models.Priority
+import com.example.todoapp.fragments.SharedViewModel
+import kotlinx.android.synthetic.main.fragment_update.view.*
+import kotlinx.android.synthetic.main.row_list.view.*
 
 class UpdateFragment : Fragment() {
     private val args by navArgs<UpdateFragmentArgs>()
+    private val mSharedViewModel: SharedViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_update, container, false)
+        view.titleTxt.text = args.currentItem.title
+        view.descriptionTxt.text = args.currentItem.description
+        view.updatePrioritySpinner.setSelection(parsePriority(args.currentItem.priority))
+        view.updatePrioritySpinner.onItemSelectedListener = mSharedViewModel.listener
+
 
         setHasOptionsMenu(true)
         return view
@@ -20,6 +31,14 @@ class UpdateFragment : Fragment() {
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
+
+    private fun parsePriority(priority: Priority): Int {
+        return when (priority) {
+            Priority.High -> 0
+            Priority.Medium -> 1
+            Priority.Low -> 2
+        }
     }
 
 }
