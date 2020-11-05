d9795577d3d28fc1d7d807a43c7dc0404ac9100e
MaedehJJ <majj2000.mail@gmail.com>
2020-11-05 19:31:07 +0330

    Fix : some bugs in Gradle

--- app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
@@ -10,9 +10,9 @@ class UpdateFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_update, container, false)
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
