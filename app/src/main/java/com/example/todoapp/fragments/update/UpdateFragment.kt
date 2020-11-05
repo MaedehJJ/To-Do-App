03f5be79044f7bdb77ec40bdfba737e9c0442269
MaedehJJ <majj2000.mail@gmail.com>
2020-11-05 19:33:16 +0330

    Add : safe Args to Update Fragment

--- app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
@@ -3,14 +3,17 @@ package com.example.todoapp.fragments.update
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.todoapp.R
 
 class UpdateFragment : Fragment() {
+    private val args by navArgs<UpdateFragmentArgs>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_update, container, false)
+
         setHasOptionsMenu(true)
         return view
     }
