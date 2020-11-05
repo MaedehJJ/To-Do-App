b3a2ae08543c39052d0ae23bef18b7d0a2ee037a
MaedehJJ <majj2000.mail@gmail.com>
2020-11-05 19:08:13 +0330

    add : safe args plugins to Gradle

--- app/src/main/java/com/example/todoapp/database/models/ToDoData.kt
+++ app/src/main/java/com/example/todoapp/database/models/ToDoData.kt
@@ -1,14 +1,17 @@
 package com.example.todoapp.database.models
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.todoapp.database.models.Priority
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity(tableName = "todo_table")
 data class ToDoData(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "Title") var title: String,
     @ColumnInfo(name = "Priority") var priority: Priority,
     @ColumnInfo(name = "Description") var description: String
-)
+): Parcelable
