1619fd1f485fa72bb64070cbd3da54d36953b29d
MaedehJJ <majj2000.mail@gmail.com>
2020-11-17 13:38:15 +0330

    Implement : Data binding To Navigate to update fragment

--- app/src/main/java/com/example/todoapp/fragments/BindingAdapter.kt
+++ app/src/main/java/com/example/todoapp/fragments/BindingAdapter.kt
@@ -5,11 +5,14 @@ import android.view.View
 import android.widget.Spinner
 import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.example.todoapp.R
 import com.example.todoapp.database.models.Priority
+import com.example.todoapp.database.models.ToDoData
+import com.example.todoapp.fragments.list.ListFragmentDirections
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.*
 
@@ -66,6 +69,16 @@ class BindingAdapter {
                 Priority.Low -> {
                     cardView.setCardBackgroundColor(cardView.context.getColor(R.color.green))
                 }
+            }
+
+        }
+
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, currentItem: ToDoData) {
+            view.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+                view.findNavController().navigate(action)
             }
 
         }
